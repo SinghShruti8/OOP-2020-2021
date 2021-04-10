08d2ec16bd7e5c584f79d33029e0d9f887f4291f
Shruti Singh <shrutisingh@Shrutis-MacBook-Air.local>
2021-04-10 10:43:46 +0100

    Monday branch

---
+++ java/src/ie/tudublin/BugZap.java
@@ -0,0 +1,19 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class BugZap extends PApplet
+{
+    public void settings()
+    {
+        size(width, height);
+    }
+
+    public void setup() {
+        
+    }
+
+    public void draw() {
+        
+    }
+}
